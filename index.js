6b212204f087b3d7b5cdeb089250949007094efb
Adam Baldwin <adamnbaldwin@gmail.com>
2023-08-24 18:47:09 -0700

    add button operators and create calculation functions

--- index.js
+++ index.js
@@ -1,8 +1,60 @@
+let val1 = ''
+let val2 = ''
+let currentOperation = null
+let shouldClearScreen = false
 
-function evaluate() {
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const lastScreen = document.getElementById('lastOppScreen')
+const currentScreen = document.getElementById('currentOppScreen')
+
+equalsButton.addEventListener('click', calculate)
+clearButton.addEventListener('click', clear)
+
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => addNumToScreen(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => prepOperation(button.textContent))
+)
+
+function addNumToScreen(number) {
+  if (currentScreen.textContent === '0' || shouldClearScreen)
+    clearScreen()
+  currentScreen.textContent += number
+}
+
+function clearScreen() {
+  currentScreen.textContent = ''
+  shouldClearScreen = false
+}
+
+function clear() {
+  currentScreen.textContent = ''
+  lastScreen.textContent = ''
+  val1 = ''
+  val2 = ''
+  currentOperation = null
+}
+
+
+function prepOperation(operator) {
+  if (currentOperation !== null) calculate()
+  val1 = currentScreen.textContent
+  currentOperation = operator
+  clearScreen();
+  lastScreen.textContent = `${val1} ${currentOperation}`;
+  shouldClearScreen = true;
+}
+
+
+
+function calculate() {
   if (currentOperation === null || shouldClearScreen) return
   if (currentOperation === '÷' && currentScreen.textContent === '0') {
-    alert("You can't divide by 0!")
     return
   }
   val2 = currentScreen.textContent
